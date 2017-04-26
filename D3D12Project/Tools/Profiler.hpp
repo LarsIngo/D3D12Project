47db4e18b10b448494b709322092246f9076ebc2
LarsIngo <ivarbjorling@hotmail.com>
2017-04-26 15:01:08 +0200

    Plot average frame time

--- D3D12Project/Tools/Profiler.hpp
+++ D3D12Project/Tools/Profiler.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <fstream>
+#include <sstream>
 
 // Profiler.
 class Profiler {
@@ -9,25 +10,32 @@ class Profiler {
         // Constructor.
         Profiler(unsigned int width, unsigned int height)
         {
-            mFileStream.open("GraphScript.m");
-
-            mFileStream << "clear all;\nclose all;\n";
-
-            mFileStream << "h = figure;\n";
-
-            mFileStream << "set(gcf, 'units', 'points', 'position', [0, 0, " + std::to_string(width) + ", " + std::to_string(height) + "]);\n";
-
-            //mStrStream >> "xlim([0 10]);\n";
-            //mStrStream >> "ylim([0 2]);\n";
-
-            mFileStream << "xlabel('x');\n";
-            mFileStream << "ylabel('y');\n";
+            mWidth = width;
+            mHeight = height;
         }
 
         // Destructor.
         ~Profiler()
         {
-            mFileStream << "saveas(h, 'file', 'png');\n";
+            mFileStream.open("GraphScript.m");
+
+            mFileStream << "clear all;\nclose all;\n";
+
+            mFileStream << "set(gcf, 'units', 'points', 'position', [0, 0, " + std::to_string(mWidth) + ", " + std::to_string(mHeight) + "]);\n";
+
+            mFileStream << "xlabel('x');\n";
+            mFileStream << "ylabel('y');\n";
+
+            mFileStream << "subplot(2,1,2);\n";
+
+            mFileStream << mRectangleStream.str();
+
+            mFileStream << "subplot(2,1,1);\n";
+            mFileStream << "hold on;\n";
+
+            mFileStream << mPointStream.str();
+
+            //plot(1493198854020035776, 1, '-ro');
 
             mFileStream.close();
 
@@ -36,9 +44,18 @@ class Profiler {
         void Rectangle( UINT64 x, UINT64 y, UINT64 width, UINT64 height,
             float r, float g, float b)
         {
-            mFileStream << "rectangle('Position', [" + std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(width) + ", " + std::to_string(height) + "], 'FaceColor', [" + std::to_string(r) + " " + std::to_string(g) + " " + std::to_string(b) + "]);\n";
+            mRectangleStream << "rectangle('Position', [" + std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(width) + ", " + std::to_string(height) + "], 'FaceColor', [" + std::to_string(r) + " " + std::to_string(g) + " " + std::to_string(b) + "]);\n";
+        }
+
+        void Point(UINT64 x, UINT64 y)
+        {
+            mPointStream << "plot(" + std::to_string(x) + ", " + std::to_string(y) + ", '-ro');\n";
         }
 
     private:
         std::ofstream mFileStream;
+        std::stringstream mRectangleStream;
+        std::stringstream mPointStream;
+        unsigned int mWidth;
+        unsigned int mHeight;
 };
